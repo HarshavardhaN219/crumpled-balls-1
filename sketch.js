488f14dd0cf886eb92730822b02c90cfb1ac1cda
HarshavardhaN219 <73733520+HarshavardhaN219@users.noreply.github.com>
2020-12-23 11:59:07 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
-var helicopterIMG, helicopterSprite, packageSprite, packageIMG;
-var packageBody;
-var box1;
 
+var log1,log2,log3;
+var ground;
+var ball;
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -9,55 +9,43 @@ const Body = Matter.Body;
 
 function preload()
 {
-	helicopterIMG=loadImage("helicopter.png")
-	packageIMG=loadImage("package.png")
+	
 }
 
 function setup() {
-	createCanvas(800, 700);
-	rectMode(CENTER);
-	
+  rectMode(CENTER);
+	createCanvas(1200, 600);
 
-	packageSprite=createSprite(width/2, 80, 20,20);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
-
-	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.6
-
-	
-
-
-	engine = Engine.create();
+  engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 20 , {restitution:0.2, isStatic:true});
-	World.add(world, packageBody);
-
-
-	
-	
-	box1 = new Box(400,690,700,20,"red");
-	
+  ground = new Ground(600,570,1200,20);
+  log1=new Dustbin(1000,550,200,20);
+	log2=new Dustbin(1090,490,20,100);
+  log3=new Dustbin(910,490,20,100);
+  ball=new  PaperBall(80,450);
+  
 	Engine.run(engine);
 }
 
 
 function draw() {
   rectMode(CENTER);
-  background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
+  background("black");
+  Engine.update(engine);
+  
+  log1.display();
+  log2.display();
+  log3.display();
+  ground.display();
+  ball.display();
+
   drawSprites();
 
-	
-  if(keyDown(DOWN_ARROW)) {
-	Matter.Body.setStatic(packageBody,false);
-	}	
-	
-	box1.display();
-	
+}
 
-  Engine.update(engine);
+function keyPressed() {
+if (keyCode=== UP_ARROW) {
+  Matter.Body.applyForce(ball.body,ball.body.position,{x:80,y:-85});
+}
 }
